90db2c927970eeac0226e02509a9fce0a3baddff
bridgetlobo <bridgettlobo@gmail.com>
2018-11-29 10:30:16 -0500

    new method
    splits a number and makes an ArrayList

--- Name.java
+++ Name.java
@@ -1,8 +1,29 @@
+import java.util.ArrayList;
+
 public class Name{
 	public static void main(String[]args){
 
 	System.out.println("Bridget");
 	System.out.println("Suchit");
 
+	System.out.println(split(12345));
 	}
+
+	public static ArrayList split(int num){
+		ArrayList <Integer> splitList = new ArrayList<>();
+		int hi = num;
+		do{
+			hi = num % 10;
+			num /= 10;
+			splitList.add(hi);
+
+		}
+		while(num > 0);
+
+		ArrayList <Integer> list2 = new ArrayList <>();
+		for(int a = splitList.size() - 1; a >= 0; a--){
+			list2.add(splitList.remove(a));
+		}
+		return list2;
+	} //split
 }
